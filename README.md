e99c2b53f0fc0d68c3538b18f842ce46d0da854b
Muhammad Usman <muhammadusman36891@gmail.com>
2024-10-06 11:59:37 +0500

    Usman

---
+++ README.md
@@ -0,0 +1,23 @@
+# condition_report_go
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+<!-- Platform  Firebase App Id
+web       1:382464457642:web:cb711f0aa01e7d71ac49d3
+android   1:382464457642:android:f9bc32cfce1aa6caac49d3
+windows   1:382464457642:web:71a9caf6b653049aac49d3 -->
+
+<!-- https://condition-report-go-92d23.firebaseapp.com/__/auth/handler -->
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:condition_report_go/Screens/bedroom1.dart';
+import 'package:condition_report_go/StartupScreens/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase core
+import 'firebase_options.dart'; // Import the generated options file
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure that Flutter is initialized before Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Initialize Firebase with the correct options
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Condition Report Go",
+      debugShowCheckedModeBanner: false,
+       // This is your signup screen
+      home: Splashscreen(),
+    );
+  }
+}
